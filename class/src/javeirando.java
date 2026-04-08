d58eac5b99211743f523e52068464db3eb81fb79
Ruan Henrique Santos Xavier <972611368@ulife.com.br>
2026-04-07 21:39:49 -0300

    Exercicios em java 7 4

---
+++ class/src/javeirando.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class javeirando {
+    public static void main(String[] args) {
+        String nomevendedor;
+        float salariofixo, valorvendas, salariototal;
+
+        Scanner receberdados = new Scanner(System.in);
+
+        System.out.println("Digite o nome do funcionario: ");
+        nomevendedor = receberdados.next();
+        System.out.println("Digite o salário mensal: ");
+        salariofixo = Float.parseFloat(receberdados.next());
+        System.out.println("Digite o valor das vendas do mês: ");
+        valorvendas = Float.parseFloat(receberdados.next());
+        salariototal = (float) (salariofixo + (valorvendas * 0.15));
+        System.out.println("Nome do vendedor é:" + nomevendedor);
+        System.out.println("Salario sem comissao é: " + salariofixo);
+        System.out.println("Salario com comissao: " + salariototal);
+    }
+}
---
+++ class/src/qualmaior.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class qualmaior {
+    public static void main(String[] args) {
+        int num1, num2,numMaior;
+                Scanner lerinteiro = new Scanner(System.in);
+                System.out.println("Digite o primeiro numero inteiro:");
+                num1 = lerinteiro.nextInt();
+                System.out.println("Digite o segundo numero inteiro");
+                num2 = lerinteiro.nextInt();
+                if(num1>num2){
+                    System.out.println("O numero 1 é maior");
+                      numMaior=num1;
+                else if (numMaior >99 & numMaior < 200){
+                    System.out.println("O segundo numero digitado é maior: ");
+        }
+    }
+}
