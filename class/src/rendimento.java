5ee1a035fe7e811dca789d9fb35929968c44bdb8
TecnoRuan <972611368@ulife.com.br>
2026-04-14 20:49:50 -0300

    Update rendimento.java

--- class/src/rendimento.java
+++ class/src/rendimento.java
@@ -6,7 +6,7 @@ public class rendimento {
         Scanner lerfloat = new Scanner(System.in);
         System.out.println("Qual o valor depositado? ");
         depositado = lerfloat.nextFloat();
-        juros = (float) (depositado + depositado*0.007);
+        juros = (float) (depositado + depositado*0.07);
         System.out.println("Valor com juros de rendimento: "+juros);
     }
 }
